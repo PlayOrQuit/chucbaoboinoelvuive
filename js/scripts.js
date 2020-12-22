ae7d6af486fef0ef0be7d82da84daabd717ebd76
dang.nh <dang.nguyenhai@gotadi.com>
2020-12-22 16:36:27 +0700

    improve

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 // 	});
 // });
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
 	//christmass balls
 	$('.ball-c a').click(function () {
 		$(this).toggleClass('open');
@@ -15,21 +15,53 @@ jQuery(document).ready(function(){
 	$.fn.snowit({
 
 		// min size of snowflake
-		minSize : 10,
-	  
+		minSize: 10,
+
 		// max size of snowflake
-		maxSize : 50,
-	  
+		maxSize: 50,
+
 		// the number of flakes generated
-		total : 30,
-	  
+		total: 30,
+
 		// speed of flakes when fall down
 		speed: documentHeight / 105,
-	  
+
 		// color of snowflake
 		// flakeColor : "#DE0603"
-		flakeColor : "#fff"
-		
-	  });
-	
+		flakeColor: "#fff"
+
+	});
+
+
+	autoOpenCloseFirst();
 });
+
+const arrClass = [".third a", ".sixth a", ".first a", ".fourth a"];
+let arrOpen = "";
+
+function autoOpenCloseFirst() {
+	setTimeout(function () {
+		jQuery(arrClass[0]).click();
+		arrOpen = arrClass[0];
+		autoDisplay();
+	}, 5000);
+}
+
+function autoDisplay() {
+	const size = arrClass.length;
+	setInterval(function(){ 
+		let index = arrClass.findIndex(checkString);
+		console.log(index);
+		jQuery(arrClass[index]).click();
+		if(index == size - 1){
+			index = -1;
+		}
+
+		jQuery(arrClass[index + 1]).click();
+		arrOpen = arrClass[index + 1];
+	}, 15000);
+}
+
+function checkString(item) {
+	return item == arrOpen;
+  }
