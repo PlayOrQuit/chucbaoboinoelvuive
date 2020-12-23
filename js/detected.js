48f6b5dc1c540905c49fa0357c169c0622184e36
dang.nh <dang.nguyenhai@gotadi.com>
2020-12-23 15:01:09 +0700

    checkmobile

--- js/detected.js
+++ js/detected.js
@@ -1,5 +1,5 @@
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     var hostname = window.location.origin;
-    window.location.href = hostname + "/index2.html";
+    window.location.href = hostname + "/chucbaoboinoelvuive/index2.html";
 }
